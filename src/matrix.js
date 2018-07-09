b11938db5b84a3b2dc99c7b9b4d8526e81984bab
zhorzh_z <zhorzh.z@bookatiger.com>
2018-07-09 17:19:50 +0200

    still not properly working version

--- src/matrix.js
+++ src/matrix.js
@@ -1,39 +1,64 @@
 const _         = require("lodash");
 const boards    = require("./maps/out/map.json");
 
-let board = boards[0];
+let sum         = 0;
+let unsolvable  = 0;
 
-let chutes = _
-    .chain(board.extras)
-    .filter(extra => extra.type === "C")
-    .keyBy("end")
-    .value();
-
-let ladders = _
-    .chain(board.extras)
-    .filter(extra => extra.type === "L")
-    .keyBy("start")
-    .value();
-
-let matrix = {};
-
-console.log("chutes", chutes);
-console.log("ladders", ladders);
-
-for (let i = 1; i < 101; i++) {
-    matrix[i] = {};
-    if(ladders[i]) {
-        matrix[i][ladders[i].end] = 1/6
-    }
-    for (let j = 1; j < 101; j++) {
-        if(j > i && j <= i+6) {
-            matrix[i][j] = 1/6;
-        }
-
-        if(!matrix[i][j]) {
-            matrix[i][j] = 0;
+_.forEach(boards, (board, board_number) => {
+    
+    let chutes = _.chain(board.extras).filter(extra => extra.type === "C").keyBy("end").value();
+    
+    let ladders = _.chain(board.extras).filter(extra => extra.type === "L").keyBy("start").value();
+    
+    let matrix = [];
+    
+    for (let i = 0; i < 101; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < 101; j++) {
+            let possible_position = j;
+            if (j <= i || j > i + 6) {
+                continue;
+            }
+            
+            if (ladders[j]) {
+                possible_position = ladders[j].end;
+            }
+            
+            if (chutes[j]) {
+                possible_position = chutes[j].start;
+            }
+            
+            matrix[i].push(possible_position);
         }
     }
-};
 
-console.log("matrix", JSON.stringify(matrix));
+// console.log("matrix", JSON.stringify(matrix));
+    
+    let finish = 100;
+    let all_rolls = [];
+    
+    for (let tries = 0; tries < 10; tries++) {
+        let current_position = 0;
+        all_rolls[tries] = 0;
+        
+        while (current_position !== finish) {
+            let block = matrix[current_position];
+            
+            if(_.isEmpty(block)) {
+                console.log("Unsolvable found", board_number);
+                unsolvable++;
+                break;
+            }
+            
+            // console.log("block", block);
+            
+            current_position = block[_.random(0, block.length - 1)];
+            all_rolls[tries]++;
+            
+            // console.log("position", current_position);
+        }
+    }
+    
+    sum += _.mean(all_rolls);
+    console.log("done", _.mean(all_rolls), sum);
+});
